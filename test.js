8308b392c2bb06fae7c7600fae06d8a39efc32bc
LectoBU <torman@bournemouth.ac.uk>
2019-03-25 01:21:04 +0000

    quintjs to quinit

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
    re-useability of JS code in both Back-end and Front-end! #isomorphic */
 /* istanbul ignore if */
 if (typeof module !== 'undefined' && module.exports) {
-    var QUnit = require('qunitjs'); // require QUnit node.js module
+    var QUnit = require('qunit'); // require QUnit node.js module
     // alias the QUnit.test method so we don't have to change all our tests
     var test = QUnit.test; // stores a copy of QUnit.test
     require('qunit-tap')(QUnit, console.log); // use console.log for test output
