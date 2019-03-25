5b4e93d4ee26eb6a22c008636648ce97ec3ae066
LectoBU <torman@bournemouth.ac.uk>
2019-03-25 01:34:33 +0000

    zzzzz

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 /* The code block below ONLY Applies to Node.js - This Demonstrates
    re-useability of JS code in both Back-end and Front-end! #isomorphic */
 /* istanbul ignore if */
+/*
 if (typeof module !== 'undefined' && module.exports) {
     var QUnit = require('qunit'); // require QUnit node.js module
     // alias the QUnit.test method so we don't have to change all our tests
@@ -8,7 +9,7 @@ if (typeof module !== 'undefined' && module.exports) {
     //require('qunit-tap')(QUnit, console.log); // use console.log for test output
     var getChange = require('./change.js'); // load our getChange method
   }
-
+*/
 test('getChange(215, 300) should return [50, 20, 10, 5]', function(assert) {
   var result = getChange(215, 300); // expect an array containing [50,20,10,5]
   var expected = [50, 20, 10, 5];
