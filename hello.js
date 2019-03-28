53a9c2c80a78a7d9f22b265ff80bd9e7fa259044
LectoBU <torman@bournemouth.ac.uk>
2019-03-28 22:56:46 +0000

    sss

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 var http = require('http');
-http.createServer(function (req, res) {
+http.createServer(function (_req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n');
-}).listen(1337, '127.0.0.1');
+}).listen(1337, '127.0.0.1')
 console.log('Server running at http://127.0.0.1:1337/');
 
